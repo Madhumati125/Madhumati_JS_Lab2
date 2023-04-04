d6aecea864eeb62280fb1f365b56c8e09beb5a56
Madhumati125 <109518311+Madhumati125@users.noreply.github.com>
2023-04-04 18:05:40 +0530

    Add files via upload

---
+++ Lab 2 Assignment/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Javascript is an _______ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answer: "Ignores the statements",
+    options: [
+      "Throws an error",
+      "Ignores the statements",
+      "Gives a warning",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above",
+    options: [
+      "document.write()",
+      "console.log()",
+      "window.alter()",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does the Javascript “debugger” statement do?",
+    answer: "It acts as a breakpoint in a program.",
+    options: [
+      "It will debug all the errors in the program at runtime.",
+      "It acts as a breakpoint in a program.",
+      "It will debug error in the current statements if any.",
+      "Allof the above."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
